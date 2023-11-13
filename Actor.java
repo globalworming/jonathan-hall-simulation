0d1031c850eefe96d15b626524ff3f419d296d6d
globalworming <andreas.worm@headissue.com>
2023-11-13 16:06:26 +0100

    half life

--- Actor.java
+++ Actor.java
@@ -2,13 +2,18 @@ import java.util.Optional;
 
 public class Actor {
 
-    private final int checkEveryMinutes;
+    private int checkEveryMinutes;
+    private final Strategy strategy;
     public int maxCheckMinute = 0;
     public int processStartedMinute = 0;
 
     public Actor(int checkEveryMinutes) {
-
+        this.strategy = Strategy.EVERY_X;
         this.checkEveryMinutes = checkEveryMinutes;
+    }
+
+    public Actor(Strategy strategy) {
+        this.strategy = strategy;
     }
 
     public Optional<Process> maybeCheck(Process process, int minute) {
@@ -19,10 +24,32 @@ public class Actor {
 
         maxCheckMinute = currentRuntime;
 
-        if (currentRuntime % checkEveryMinutes == 0) {
-            return Optional.of(process.check());
+        switch (strategy) {
+            case EVERY_X -> {
+                if (currentRuntime % checkEveryMinutes == 0) {
+                    return Optional.of(process.check());
+                }
+            }
+            case HALF_LIFE -> {
+                if (currentRuntime == 90) return Optional.of(process.check());
+                if (currentRuntime == 83) return Optional.of(process.check());
+                if (currentRuntime == 67) return Optional.of(process.check());
+                if (currentRuntime == 45) return Optional.of(process.check());
+                if (currentRuntime == 22) return Optional.of(process.check());
+
+            }
         }
         return Optional.empty();
 
     }
+
+
+    public enum Strategy {
+
+        EVERY_X, HALF_LIFE
+
+    }
+
 }
+
+
--- Simulation.java
+++ Simulation.java
@@ -10,7 +10,6 @@ public class Simulation {
             List<Integer> doneMinutes = new ArrayList<>();
             for (int i = 0; i < 100000; i++) {
                 int minute = 0;
-                int interruptions = 0;
                 var process = new Process(random);
                 Actor actor = new Actor(checkEveryMinutes);
                 while (minute < 1000) {
@@ -24,7 +23,6 @@ public class Simulation {
                             break;
                         }
                         if (result.get().interrupted) {
-                            interruptions++;
                             process = new Process(random, result.get().runtimeMinutes);
                             actor.processStartedMinute = minute;
                         }
@@ -33,6 +31,31 @@ public class Simulation {
             }
             System.out.println("check every " + checkEveryMinutes + " minutes, mean over 100000 processes " + doneMinutes.stream().mapToInt(it -> it).average().orElse(0));
         }
+
+        var random = new Random();
+        List<Integer> doneMinutes = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            int minute = 0;
+            var process = new Process(random);
+            Actor actor = new Actor(Actor.Strategy.HALF_LIFE);
+            while (minute < 1000) {
+                minute++;
+                process.progress();
+                var result = actor.maybeCheck(process, minute);
+                if (result.isPresent()) {
+                    if (result.get().done) {
+                        doneMinutes.add(minute);
+                        //System.out.printf("%s, %s, %s, %s%n", result.get().runtimeMinutes, "every five minutes", interruptions, minute);
+                        break;
+                    }
+                    if (result.get().interrupted) {
+                        process = new Process(random, result.get().runtimeMinutes);
+                        actor.processStartedMinute = minute;
+                    }
+                }
+            }
+        }
+        System.out.println("half-life: check at 90,83,67,45,22" +" minute, mean over 100000 processes " + doneMinutes.stream().mapToInt(it -> it).average().orElse(0));
     }
 
 }
